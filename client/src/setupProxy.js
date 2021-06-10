c1258e1d237e4035f63345cdf548f13379112ba0
Iosismu <evanpark333@gmail.com>
2021-06-10 17:21:55 +0900

    heroku deployment proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000", // client에서 타켓을 줄때 5000번으로 주겠다는 말
+      target: "https://fathomless-savannah-85923.herokuapp.com/", // client에서 타켓을 줄때 5000번으로 주겠다는 말
       changeOrigin: true,
     })
   );
