27b8e0a15ca738a4ca6ca0c2dea18a5e87f65765
Iosismu <evanpark333@gmail.com>
2021-06-10 18:45:24 +0900

    heroku deployment logout status

--- server/index.js
+++ server/index.js
@@ -109,7 +109,7 @@ app.get("/api/users/auth", auth, (req, res) => {
 app.get("/api/users/logout", auth, (req, res) => {
   User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
     if (err) return res.json({ sucess: false, err });
-    return res.status(200).send({
+    return res.status(304).send({
       success: true,
     });
   });
