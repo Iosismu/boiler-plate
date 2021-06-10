8ef901deb765b021a9719b2bb377076908ece06b
Iosismu <evanpark333@gmail.com>
2021-06-10 18:26:46 +0900

    heroku deployment ch

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
-const path = require("path");
 
 // Server static assets if in production
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
   app.use(express.static("client/build"));
 
+  // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
